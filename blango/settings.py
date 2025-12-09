445ab95249f00b68c17801442fa9f57dacc6f616
Francisco Francisco <francisco.del.campo.r@gmail.com>
2025-12-09 22:29:58 +0000

    Finish Serializers

--- blango/settings.py
+++ blango/settings.py
@@ -49,6 +49,7 @@ class Dev(Configuration):
         'django.contrib.messages',
         'django.contrib.sites',
         'django.contrib.staticfiles',
+        'rest_framework',
         'blango_auth',
         'blog',
         'crispy_forms',
